896f103424516ba8323d87c0b238cd52e4d88d49
Radhames Capellan <radhamesc@gmail.com>
2025-04-12 18:56:53 +0200

    inicializacion del proyecto | clase Usuario y Clase EntornosFactorizar

---
+++ Refactorizacion_ControlVersiones/src/prueba/Usuario.java
@@ -0,0 +1,51 @@
+package prueba;
+
+public class Usuario {
+    private String nombre;
+    private String email;
+    private boolean empleado;
+    private boolean miembroGold;
+    private boolean miembroSilver;
+
+ 
+    public Usuario(String nombre, String email, boolean empleado, boolean miembroGold, boolean miembroSilver) {
+        this.nombre = nombre;
+        this.email = email;
+        this.empleado = empleado;
+        this.miembroGold = miembroGold;
+        this.miembroSilver = miembroSilver;
+    }
+
+   
+    public boolean esEmpleado() {
+        return empleado;
+    }
+
+    public boolean esMiembroGold() {
+        return miembroGold;
+    }
+
+    public boolean esMiembroSilver() {
+        return miembroSilver;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", empleado=" + empleado +
+                ", miembroGold=" + miembroGold +
+                ", miembroSilver=" + miembroSilver +
+                '}';
+    }
+}
